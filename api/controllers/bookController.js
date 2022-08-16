85d6fd6d443f531f2bfea95373228a0e5fc79f79
servo pcerda <fernandonando98@live.com.mx>
2022-08-15 21:34:54 -0500

    create book

--- api/controllers/bookController.js
+++ api/controllers/bookController.js
@@ -1,9 +1,24 @@
+import Book from '../models/Book.js';
+
+const createBook = async (req, res) => {
+  try {
+    const newBook = await Book.create(req.body);
+    return res.json({
+      msg: 'Libro creado',
+      book: newBook,
+    });
+  } catch (error) {
+    return res.json({
+      msg: 'Error al crear libro',
+    });
+  }
+};
+
 const getAllBooks = (req, res) => {
   return res.json({
     msg: 'Esta es la ruta GET  de todos los libros',
   });
 };
-const createBook = (req, res) => {};
 const getBookById = (req, res) => {};
 const updateBookById = (req, res) => {};
 const deleteBookById = (req, res) => {};
